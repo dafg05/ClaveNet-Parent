f55a414b86f7c2bba0449b4fbc2ef572e6f489a4
dafg05 <danialefloresg@gmail.com>
2024-03-22 20:35:23 -0400

    untested training pipeline

--- datafun.py
+++ datafun.py
@@ -6,12 +6,20 @@ from datetime import datetime
 from tqdm import tqdm
 
 from preprocessing import preprocessing as pre
+import learning.training as training
 
 DATA_TO_CLUSTER_SCRIPT = './dataToCluster.sh'
-RUNS_DIR = 'runs'
+PREPROCESSING_RUNS_DIR = 'preprocessing_runs'
 PREPROCESSING_OUT = 'pre_out'
-ERROR_LOGS = 'run_errors.log'
+TRAINING_ERROR_LOGS = 'preprocessing_errors.log'
 PREPROCESSED_KEYWORD = 'PreProcessed'
+
+TRAINING_RUNS_DIR = 'train_runs'
+TRAINING_OUT = 'train_out'
+TRAINING_ERROR_LOGS = 'train_errors.log'
+HYPERPARAMS_SETTING = 'solar-shadow'
+LOG_WANDB = True
+IS_SMOL = True
 
 NUM_TRANSFORMATION_VALUES = [1]
 NUM_REPLACEMENT_VALUES = [1, 2, 4]
@@ -22,14 +30,17 @@ TEST_REPLACEMENT_VALUES = [1,2]
 TEST_OUT_OF_STYLE_PROB_VALUES = [0.5]
 
 def pre_processing_pipeline(test=True):
+    # Preprocess dataset with different data augmentation parameters.
+    # NOTE: Unfortunately, this pipeline can only be run on the local machine.
+
     # name each run with a timestamp
     time_str = str(int(datetime.now().timestamp()))
-    run_path = Path(RUNS_DIR, time_str)
+    run_path = Path(PREPROCESSING_RUNS_DIR, time_str)
     Path.mkdir(run_path, exist_ok=True)
 
-    preprocess_run_dir = Path(run_path, PREPROCESSING_OUT)
-    error_log_path = Path(run_path, ERROR_LOGS)
+    preprocess_out_dir = Path(run_path, PREPROCESSING_OUT)
 
+    error_log_path = Path(run_path, TRAINING_ERROR_LOGS)
     with open(error_log_path, 'w') as f:
         f.write(f"Preprocessing error log for run {time_str} \n")
 
@@ -42,11 +53,11 @@ def pre_processing_pipeline(test=True):
     for data_aug_params in tqdm(combinations, desc="Processing pipeline"):
         try:
             # preprocess the dataset
-            pre.preprocess(preprocess_run_dir, data_aug_params)
-            assert len(get_preprocessed_datasets(preprocess_run_dir)) == 1, "More than one preprocessed dataset found."
+            pre.preprocess(preprocess_out_dir, data_aug_params)
+            assert len(get_preprocessed_datasets(preprocess_out_dir)) == 1, "More than one preprocessed dataset found."
         
             # run dataToCluster.sh to copy the preprocessed dataset to hpc
-            preprocessed_dataset = get_preprocessed_datasets(preprocess_run_dir)[0]
+            preprocessed_dataset = get_preprocessed_datasets(preprocess_out_dir)[0]
             result = subprocess.run([DATA_TO_CLUSTER_SCRIPT, preprocessed_dataset, time_str], capture_output=True, text=True)
             if result.returncode != 0:
                 raise Exception(f"Error running dataToCluster.sh; error: {result.stderr}")
@@ -59,10 +70,41 @@ def pre_processing_pipeline(test=True):
 
         finally:
             # clear the preprocess_run_dir
-            clear_dir(preprocess_run_dir)
+            clear_dir(preprocess_out_dir)
 
     
     print(f"Preprocessed {len(combinations) - error_count} out of {len(combinations)} combinations. Errors written on {error_log_path}. Check cluster for processed datasets.")
+
+def train_pipeline(preprocessed_datasets_paths, out_models_dir):
+    # Process the preprocessed datasets on the cluster, and then train the models.
+
+    # name each run with a timestamp
+    time_str = str(int(datetime.now().timestamp()))
+    run_path = Path(TRAINING_RUNS_DIR, time_str)
+    Path.mkdir(run_path, exist_ok=True)
+
+    error_log_path = Path(run_path, TRAINING_ERROR_LOGS)
+    with open(error_log_path, 'w') as f:
+        f.write(f"Training error log for run {time_str} \n")
+
+    error_count = 0
+    for ppd_path in tqdm(preprocessed_datasets_paths, desc="Training pipeline"):
+        try:
+            # process the preprocessed dataset
+            pd_path = training.process(ppd_path, TRAINING_RUNS_DIR)
+            # train the model
+            model_path = training.train(HYPERPARAMS_SETTING, pd_path, out_models_dir, LOG_WANDB, IS_SMOL)
+
+        except Exception as e:
+            print("An error occured while training the model.")
+            with open(error_log_path, 'a') as f:
+                f.write(f"Error training with preprocessed dataset: {ppd_path}. Stack trace: {e} \n")
+                error_count += 1
+
+        finally:
+            # delete the processed dataset
+            shutil.rmtree(pd_path)
+    
 
 def evaluation_pipeline():
     pass
@@ -99,4 +141,15 @@ def get_preprocessed_datasets(preprocess_run_dir):
 
 
 if __name__ == "__main__":
-    pre_processing_pipeline()
+    # Take in an argument to specify whether to run the preprocessing pipeline or the training pipeline.
+    # If no argument is given, show usage and exit.
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python datafun.py <preprocess/train>")
+        sys.exit(1)
+    
+    if sys.argv[1] == 'preprocess':
+        pre_processing_pipeline()
+    elif sys.argv[1] == 'train':
+        preprocessed_datasets = get_preprocessed_datasets(Path('../preprocessed_datasets'))
+        train_pipeline(preprocessed_datasets, TRAINING_RUNS_DIR)
